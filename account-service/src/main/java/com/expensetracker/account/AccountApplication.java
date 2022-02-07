0bcb4ed6bd7b65ab3a039a69e52fbcdbd3ba6ba4
Douglas <douglaschinwuba10@gmail.com>
2022-02-06 19:41:06 -0500

    created account service with dependencies

---
+++ account-service/src/main/java/com/expensetracker/account/AccountApplication.java
@@ -0,0 +1,8 @@
+package com.expensetracker.account;
+
+public class AccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+}
