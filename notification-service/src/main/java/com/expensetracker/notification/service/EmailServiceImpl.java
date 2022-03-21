9178edf8c16ae24d09026da6cd3229fca3bd43e3
Douglas <douglaschinwuba10@gmail.com>
2022-03-20 23:06:24 -0400

    Added email authentication for email notification

--- notification-service/src/main/java/com/expensetracker/notification/service/EmailServiceImpl.java
+++ notification-service/src/main/java/com/expensetracker/notification/service/EmailServiceImpl.java
@@ -4,11 +4,14 @@ import com.expensetracker.notification.model.Recipient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -23,19 +26,32 @@ public class EmailServiceImpl implements EmailService{
     @Autowired
     private RecipientServiceImpl recipientService;
 
-//    @Scheduled(cron = "${notify.cron}")
-    @Scheduled(fixedRate = 2000L)
+    @Value("${expenseTracker.notification.email}")
+    private String from;
+
+    @Value("${expenseTracker.notification.password}")
+    private String password;
+
+    @Scheduled(cron = "${notify.cron}")
     @Override
     public void sendNotification(){
-        final String from = "expensetracker@mail.com";
-
-        String host = "127.0.0.1";
+        String host = "smtp.gmail.com";
         String port = "587";
+
+        Authenticator auth = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        };
 
         Properties properties = System.getProperties();
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", port);
-        Session session = Session.getDefaultInstance(properties);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getDefaultInstance(properties, auth);
 
         try {
             for(Recipient recipient : recipientService.findAllRecipients()){
@@ -45,7 +61,7 @@ public class EmailServiceImpl implements EmailService{
                         new InternetAddress(recipient.getUser().getEmail()));
                 message.setSubject("Expense tracker account notification");
 
-                String text = String.format("Hi %s,\n Your balance is $%.2f, your total income is $%.2f and total expense is $%.2f",
+                String text = String.format("Hi %s,\n\n Your balance is $%.2f, your total income is $%.2f and total expense is $%.2f",
                                             recipient.getUser().getFirstname(), recipient.getBalance(), recipient.getIncome(), recipient.getExpense());
                 message.setText(text);
                 Transport.send(message);
